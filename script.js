953bfcca6e1e8d4aada6cef590c7dcc17bec2a8c
isaach55 <isaach55@nycstudents.net>
2024-10-11 12:28:36 -0400

    aaa

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById(""),
   input: document.querySelector(`#`),
+  text: document.querySelector("#text"),
 };
+
+function Text(text) {
+  text.textContent = "text";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  Text(DOMSelectors.text);
+});
