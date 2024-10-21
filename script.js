3578bd9415f36e603515c94f624f4902a6caebe9
isaach55 <isaach55@nycstudents.net>
2024-10-21 00:54:36 -0400

    formatting css

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-//holds dom selector
 const DOMSelectors = {
   form: document.getElementById("form"),
   formInput: document.getElementById("input"),
   formDescription: document.getElementById("description"),
+  formImage: document.getElementById("imageURL"),
   submitButton: document.getElementById("submit"),
   list: document.getElementById("list")
 };
 
-//create function
 function createDiv() {
     const div = {
+        image: DOMSelectors.formImage.value,
         name: DOMSelectors.formInput.value,
-        description: DOMSelectors.formDescription.value
+        description: DOMSelectors.formDescription.value,
     };
 
-    //call inject function, clear function
     inject(div);
     clearInputs();
 }
@@ -23,12 +22,12 @@ function inject(div) {
     const item = document.createElement("div");
     item.className = "item"
     item.innerHTML = `
-        <p class="box">${div.name}</p>
-        <p class="box">${div.description}</p>
+        <img src="${div.image}" alt="Inputted Image">
+        <p class="divName">${div.name}</p>
+        <p class="divDescription">Description: ${div.description}</p>
         <button class="removeButton">Remove</button>
     `;
     DOMSelectors.list.appendChild(item)
-    item.style.backgroundColor = "#94BFA7"
     item.querySelector(".removeButton").addEventListener("click", () => {
         removeDiv(item);
     }
@@ -38,13 +37,13 @@ function inject(div) {
 function clearInputs() {
     DOMSelectors.formInput.value = "";
     DOMSelectors.formDescription.value = "";
+    DOMSelectors.formImage.value = "";
 }
 
 function removeDiv(item) {
     DOMSelectors.list.removeChild(item);
 }
 
-//listens for submit button click
 DOMSelectors.submitButton.addEventListener("click", (event) => {
     event.preventDefault(); // Prevents the form from submitting
     createDiv();
